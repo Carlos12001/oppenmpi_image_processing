4a6e285c413304a5798d1017958dd955059cb6c5
Ignacio Grané <ignaciograner@gmail.com>
2024-10-31 23:57:54 -0600

    Checkpoint: Displaying all numbers

--- driver/gpio_driver.c
+++ driver/gpio_driver.c
@@ -29,8 +29,8 @@ static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, l
 	to_copy = min(count, sizeof(tmp));
 
 	/* Read value of button */
-	printk("Value of button: %d\n", gpio_get_value(17));
-	tmp[0] = gpio_get_value(17) + '0';
+	//printk("Value of button: %d\n", gpio_get_value(17));
+	//tmp[0] = gpio_get_value(17) + '0';
 
 	/* Copy data to user */
 	not_copied = copy_to_user(user_buffer, &tmp, to_copy);
@@ -48,6 +48,34 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
 	int to_copy, not_copied, delta;
 	char value;
 
+	const int gpio_pins[] = {2, 3, 4, 5, 6, 7, 8};
+	const int num_pins = sizeof(gpio_pins) / sizeof(gpio_pins[0]);
+
+	/* Tabla de valores para cada dígito '0' a '9' */
+	const char gpio_values[10][7] = {
+		// GPIO2, GPIO3, GPIO4, GPIO5, GPIO6, GPIO7, GPIO8
+		// '0'
+		{0, 1, 1, 1, 1, 1, 1},
+		// '1'
+		{0, 0, 0, 1, 0, 0, 1},
+		// '2'
+		{1, 0, 1, 1, 1, 1, 0},
+		// '3'
+		{1, 0, 1, 1, 0, 1, 1},
+		// '4'
+		{1, 1, 0, 1, 0, 0, 1},
+		// '5'
+		{1, 1, 1, 0, 0, 1, 1},
+		// '6'
+		{1, 1, 1, 0, 1, 1, 1},
+		// '7'
+		{0, 0, 1, 1, 0, 0, 1},
+		// '8'
+		{1, 1, 1, 1, 1, 1, 1},
+		// '9'
+		{1, 1, 1, 1, 0, 1, 1}
+	};
+
 	/* Get amount of data to copy */
 	to_copy = min(count, sizeof(value));
 
@@ -55,17 +83,17 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
 	not_copied = copy_from_user(&value, user_buffer, to_copy);
 
 	/* Setting the LED */
-	switch(value) {
-		case '0':
-			gpio_set_value(4, 0);
-			break;
-		case '1':
-			gpio_set_value(4, 1);
-			break;
-		default:
-			printk("Invalid Input!\n");
-			break;
-	}
+	/* Verificar si el valor está entre '0' y '9' */
+    	if (value >= '0' && value <= '9') {
+        	int digit = value - '0'; // Convertir carácter a índice (0-9)
+
+        	/* Iterar sobre cada pin GPIO y establecer su valor correspondiente */
+	        for (int i = 0; i < num_pins; i++) {
+        	    gpio_set_value(gpio_pins[i], gpio_values[digit][i]);
+        	}
+    	} else {
+        	printk(KERN_WARNING "Entrada inválida: %c\n", value);
+	    }
 
 	/* Calculate data */
 	delta = to_copy - not_copied;
@@ -130,6 +158,38 @@ static int __init ModuleInit(void) {
 		printk("Registering of device to kernel failed!\n");
 		goto AddError;
 	}
+	
+	
+	
+	
+	
+	
+	// GPIOS
+	/* GPIO 2 init */
+	if(gpio_request(2, "rpi-gpio-2")) {
+		printk("Can not allocate GPIO 2\n");
+		goto AddError;
+	}
+
+	/* Set GPIO 2 direction */
+	if(gpio_direction_output(2, 0)) {
+		printk("Can not set GPIO 2 to output!\n");
+		goto Gpio2Error;
+	}
+	
+	
+	/* GPIO 3 init */
+	if(gpio_request(3, "rpi-gpio-3")) {
+		printk("Can not allocate GPIO 3\n");
+		goto AddError;
+	}
+
+	/* Set GPIO 3 direction */
+	if(gpio_direction_output(3, 0)) {
+		printk("Can not set GPIO 3 to output!\n");
+		goto Gpio3Error;
+	}
+	
 
 	/* GPIO 4 init */
 	if(gpio_request(4, "rpi-gpio-4")) {
@@ -142,25 +202,97 @@ static int __init ModuleInit(void) {
 		printk("Can not set GPIO 4 to output!\n");
 		goto Gpio4Error;
 	}
+	
+	
+	/* GPIO 5 init */
+	if(gpio_request(5, "rpi-gpio-5")) {
+		printk("Can not allocate GPIO 5\n");
+		goto AddError;
+	}
 
-	/* GPIO 17 init */
+	/* Set GPIO 5 direction */
+	if(gpio_direction_output(5, 0)) {
+		printk("Can not set GPIO 5 to output!\n");
+		goto Gpio5Error;
+	}
+	
+	
+	/* GPIO 6 init */
+	if(gpio_request(6, "rpi-gpio-6")) {
+		printk("Can not allocate GPIO 6\n");
+		goto AddError;
+	}
+
+	/* Set GPIO 6 direction */
+	if(gpio_direction_output(6, 0)) {
+		printk("Can not set GPIO 6 to output!\n");
+		goto Gpio6Error;
+	}
+	
+	
+	/* GPIO 7 init */
+	if(gpio_request(7, "rpi-gpio-7")) {
+		printk("Can not allocate GPIO 7\n");
+		goto AddError;
+	}
+
+	/* Set GPIO 7 direction */
+	if(gpio_direction_output(7, 0)) {
+		printk("Can not set GPIO 7 to output!\n");
+		goto Gpio7Error;
+	}
+	
+	
+	/* GPIO 8 init */
+	if(gpio_request(8, "rpi-gpio-8")) {
+		printk("Can not allocate GPIO 8\n");
+		goto AddError;
+	}
+
+	/* Set GPIO 8 direction */
+	if(gpio_direction_output(8, 0)) {
+		printk("Can not set GPIO 8 to output!\n");
+		goto Gpio8Error;
+	}
+	
+	
+	
+
+	/*
+	// GPIO 17 init 
 	if(gpio_request(17, "rpi-gpio-17")) {
 		printk("Can not allocate GPIO 17\n");
 		goto Gpio4Error;
 	}
 
-	/* Set GPIO 17 direction */
+	// Set GPIO 17 direction
 	if(gpio_direction_input(17)) {
 		printk("Can not set GPIO 17 to input!\n");
 		goto Gpio17Error;
 	}
+	* */
+
+
+
+
 
 
 	return 0;
-Gpio17Error:
-	gpio_free(17);
+
+Gpio2Error:
+	gpio_free(2);
+Gpio3Error:
+	gpio_free(3);
 Gpio4Error:
 	gpio_free(4);
+Gpio5Error:
+	gpio_free(5);
+Gpio6Error:
+	gpio_free(6);
+Gpio7Error:
+	gpio_free(7);
+Gpio8Error:
+	gpio_free(8);
 AddError:
 	device_destroy(my_class, my_device_nr);
 FileError:
@@ -174,9 +306,20 @@ ClassError:
  * @brief This function is called, when the module is removed from the kernel
  */
 static void __exit ModuleExit(void) {
+	gpio_set_value(2, 0);
+	gpio_set_value(3, 0);
 	gpio_set_value(4, 0);
-	gpio_free(17);
+	gpio_set_value(5, 0);
+	gpio_set_value(6, 0);
+	gpio_set_value(7, 0);
+	gpio_set_value(8, 0);
+	gpio_free(2);
+	gpio_free(3);
 	gpio_free(4);
+	gpio_free(5);
+	gpio_free(6);
+	gpio_free(7);
+	gpio_free(8);
 	cdev_del(&my_device);
 	device_destroy(my_class, my_device_nr);
 	class_destroy(my_class);
